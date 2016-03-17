55488ec714377dbc9e353ca169efb660edc5076e
iNfame <vyacheslav.anosov@adwatch.ru>
2016-03-17 18:59:07 +0300

    watch tasks + bundles generator started

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,10 +1,25 @@
+'use strict';
+
 import del from "del";
+import fs from "fs";
 import path from "path";
 import gulp from "gulp";
 import open from "open";
+import rimraf from "rimraf";
+import _ from "lodash";
+import source from "vinyl-source-stream";
+import buffer from "vinyl-buffer";
 import gulpLoadPlugins from "gulp-load-plugins";
 import packageJson from "./package.json";
 import runSequence from "run-sequence";
+
+import browserSync from "browser-sync";
+let reload = browserSync.reload;
+let bs = browserSync.create();
+
+import watchify from "watchify";
+import browserify from "browserify";
+import babelify from "babelify";
 
 const PORT = process.env.PORT || 3000;
 const $ = gulpLoadPlugins({camelize: true});
@@ -17,6 +32,33 @@ var sassConfig = {
 	bowerDir: './bower_components'
 };
 
+// Misc Functions
+var bundles = [];
+let checkJsBundles = () => {
+	let p = SOURCES_DIR + '/js/';
+	fs.readdir(p, (err, files)  => {
+		if (err){
+			throw err;
+		}
+
+		files.map((file) => {
+			return path.join(p, file)
+		}).filter((file) => {
+			return fs.statSync(file).isFile();
+		}).forEach((file) => {
+			console.log("%s (%s)", file, path.extname(file))
+		})
+	})
+};
+
+let genJsBundles = (config) => {
+
+};
+
+
+
+
+// Main Tasks
 gulp.task('serve', () => runSequence('serve:clean', 'serve:start'));
 gulp.task('dist', () => runSequence('dist:clean', 'dist:build'));
 gulp.task('clean', ['dist:clean, serve:clean']);
@@ -38,7 +80,8 @@ gulp.task('serve:sass', () => {
 			return 'Error: ' + error.message;
 		})))
 		.pipe($.sourcemaps.write('.'))
-		.pipe(gulp.dest(PUBLIC_DIR + '/css'));
+		.pipe(gulp.dest(PUBLIC_DIR + '/css'))
+		.pipe(browserSync.reload({stream:true}))
 });
 
 gulp.task('dist:sass', () => {
@@ -50,7 +93,8 @@ gulp.task('dist:sass', () => {
 			return 'Error: ' + error.message;
 		})))
 		.pipe($.sourcemaps.write('.'))
-		.pipe(gulp.dest(PUBLIC_DIR + '/css'));
+		.pipe(gulp.dest(PUBLIC_DIR + '/css'))
+		.pipe(browserSync.reload({stream:true}))
 });
 
 gulp.task('serve:jade', () => {
@@ -61,7 +105,8 @@ gulp.task('serve:jade', () => {
 			return 'Error: ' + error.message;
 		})))
 		.pipe($.prettify({indent_char: '	', indent_size: 1}))
-		.pipe(gulp.dest(PUBLIC_DIR + '/'));
+		.pipe(gulp.dest(PUBLIC_DIR + '/'))
+		.pipe(browserSync.reload({stream:true}));
 });
 
 gulp.task('dist:jade', () => {
@@ -72,6 +117,81 @@ gulp.task('dist:jade', () => {
 			return 'Error: ' + error.message;
 		})))
 		.pipe($.prettify({indent_char: '	', indent_size: 1}))
-		.pipe(gulp.dest(PUBLIC_DIR + '/'));
+		.pipe(gulp.dest(PUBLIC_DIR + '/'))
+		.pipe(browserSync.reload({stream:true}));
 });
 
+gulp.task('serve:start', ['serve:sass', 'serve:jade', 'watch', 'web-bs'], () => {
+	checkJsBundles();
+});
+
+// browserify
+
+let bundler;
+
+let browserifyConfig = {
+	entryFile: SOURCES_DIR + '/js/main.js',
+	outputDir: PUBLIC_DIR + '/js/',
+	outputFile: 'es6.js'
+};
+
+function getBundler() {
+	if (!bundler) {
+		bundler = watchify(browserify(browserifyConfig.entryFile, _.extend({ debug: true }, watchify.args)));
+	}
+	return bundler;
+}
+
+function bundle() {
+	return getBundler()
+		.transform(babelify)
+		.bundle()
+		.on('error', function(err) { console.log('Error: ' + err.message); })
+		.pipe(source(browserifyConfig.outputFile))
+		.pipe(gulp.dest(browserifyConfig.outputDir))
+		.pipe(reload({ stream: true }));
+}
+
+gulp.task('js-clean', function(cb){
+	rimraf(browserifyConfig.outputDir, cb);
+	// todo: переписать
+});
+
+gulp.task('build-persistent', ['js-clean'], function() {
+	return bundle();
+});
+
+gulp.task('build', ['build-persistent'], function() {
+	process.exit(0);
+});
+
+gulp.task('web-bs', ['build-persistent'], function() {
+
+	browserSync({
+		server: {
+			baseDir: PUBLIC_DIR
+		}
+	});
+
+	getBundler().on('update', function() {
+		gulp.start('build-persistent')
+	});
+});
+
+// WEB SERVER
+gulp.task('web', function () {
+	browserSync({
+		server: {
+			baseDir: PUBLIC_DIR
+		}
+	});
+});
+
+gulp.task('watch', () => {
+	gulp.watch([SOURCES_DIR + '/css/*.scss'], ['serve:sass']);
+	gulp.watch([SOURCES_DIR + '/*.jade'], ['serve:jade']);
+	//gulp.watch([SOURCES_DIR + '/js/*.js'], ['serve:js']);
+	//gulp.watch([SOURCES_DIR + '/css/*.scss'], ['serve:sass']);
+	//gulp.watch([SOURCES_DIR + '/css/*.scss'], ['serve:sass']);
+	//gulp.watch([SOURCES_DIR + '/css/*.scss'], ['serve:sass']);
+});
