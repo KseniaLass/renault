5eb1079ab0ccdd1077293969d961164fedd6c854
ksenialass@gmail.com <Lass@MacBook-Air-Ksenia.local>
2016-11-06 22:33:20 +0300

    ready + some JS

--- sources/js/main.js
+++ sources/js/main.js
@@ -1,1 +1,40 @@
 // main bundle >> main.bundle.js
+
+console.log('ok')
+
+
+// Show mobile menu
+
+$(document).on('click','.mobile-button', (e) => {
+    $('.wrapper').toggleClass('show-menu');
+});
+
+// Show mobile list on collection
+
+function collectionList() => {
+    $('.tabs ul').toggleClass('open');
+    console.log('fff')
+}
+
+$(document).on('click', '.tabs ul li.active', (e) => {
+    collectionList();
+})
+
+// Change tabs on collection
+
+$(document).on('click', '.tabs ul li', (e) => {
+    let li = $(e.target).closest('li'),
+        li_list = $(li).siblings('li'),
+        li_first = li_list[0];
+
+    if (!$(li).hasClass('active')) {
+        $(li).addClass('active').siblings('li').removeClass('active');
+        collectionList();
+
+        if ($(window).width() < 1024) {
+            $(li).insertBefore($(li_first));
+        }
+    }
+
+
+})
