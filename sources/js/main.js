e5d7701901569dfa288b856dc6cb173c80d2fb61
Ksenia Dekhonova <Ksenia.Dekhonova@t-agency.ru>
2016-11-07 14:07:05 +0300

    'ready'

--- sources/js/main.js
+++ sources/js/main.js
@@ -1,40 +1,25 @@
 // main bundle >> main.bundle.js
 
-console.log('ok')
-
-
 // Show mobile menu
-
-$(document).on('click','.mobile-button', (e) => {
-    $('.wrapper').toggleClass('show-menu');
+$(function() {
+    $('.mobile-button').on('click', () => {
+        $('.wrapper').toggleClass('show-menu');
+    });
 });
 
 // Show mobile list on collection
+$(function() {
+    $('.tabs ul li').on('click', (e) => {
+        let li = $(e.target).closest('li'),
+            li_list = $(li).siblings('li'),
+            li_first = li_list[0],
+            data_link = $(li).data('link');
 
-function collectionList() => {
-    $('.tabs ul').toggleClass('open');
-    console.log('fff')
-}
-
-$(document).on('click', '.tabs ul li.active', (e) => {
-    collectionList();
-})
-
-// Change tabs on collection
-
-$(document).on('click', '.tabs ul li', (e) => {
-    let li = $(e.target).closest('li'),
-        li_list = $(li).siblings('li'),
-        li_first = li_list[0];
-
-    if (!$(li).hasClass('active')) {
         $(li).addClass('active').siblings('li').removeClass('active');
-        collectionList();
-
+        $('.tab[data-link="'+data_link+'"]').addClass('active').siblings().removeClass('active');
+        $('.tabs ul').toggleClass('open');
         if ($(window).width() < 1024) {
             $(li).insertBefore($(li_first));
         }
-    }
-
-
-})
+    })
+});
