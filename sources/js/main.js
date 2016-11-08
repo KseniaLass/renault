3fd31d9703ee6f97e641a8c7d3582eb83c7ee7da
Ksenia Dekhonova <Ksenia.Dekhonova@t-agency.ru>
2016-11-08 19:18:15 +0300

    Add animation

--- sources/js/main.js
+++ sources/js/main.js
@@ -23,3 +23,71 @@ $(function() {
         }
     })
 });
+
+// Form input
+$(function() {
+   $('.for-input input').on('focus', (e) => {
+       let input = $(e.target),
+           parent = $(input).closest('.for-input');
+       $(parent).addClass('active');
+   });
+    $('.for-input input').on('blur', (e) => {
+        let input = $(e.target),
+            parent = $(input).closest('.for-input'),
+            label = $(parent).find('label'),
+            val = $(input).val();
+        if(val.length == 0) {
+            $(parent).removeClass('active');
+        }
+    });
+});
+
+// Open/close modal
+$(function() {
+    $('.popup-link').on('click', (e) => {
+        let modal = $('.modal'),
+            overlay = $('.overlay');
+        $(modal).addClass('showModal on');
+        $('.wrapper').addClass('show-modal');
+        setTimeout(() => $(modal).removeClass('showModal'), 500);
+    });
+    $('.overlay').on('click', (e) => {
+        let modal = $('.modal'),
+            overlay = $('.overlay');
+        $(modal).addClass('hideModal');
+        $('.wrapper').removeClass('show-modal');
+        setTimeout(() => $(modal).removeClass('hideModal on'), 500);
+    });
+});
+
+// Form validation
+$(function(){
+    let removEr = () => {
+        $('input').focus(function(){
+            $(this).removeClass('shakeError error');
+        });
+    };
+    $('.reg-form').on('submit', (e) => {
+        let form = $(e.target),
+            login = $(form).find('input[name="login"]'),
+            pass = $(form).find('input[name="pass"]'),
+            error = false;
+
+        if(login.val().length < 3) {
+            $(login).addClass('shakeError error');
+            removEr();
+            error = true;
+        }
+        if(pass.val().length < 3) {
+            $(pass).addClass('shakeError error');
+            removEr();
+            error = true;
+        }
+        if (error) {
+            console.log('err')
+            return false;
+        } else {
+            console.log('ok')
+        }
+    })
+})
