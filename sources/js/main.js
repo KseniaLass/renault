fe10bd78bb27b586e2d8d09d781d8eb29200431b
KDekho01 <ksenia.dekhonova@isobar.ru>
2016-11-10 17:43:04 +0300

    Refactor after check

--- sources/js/main.js
+++ sources/js/main.js
@@ -1,93 +1,80 @@
 // main bundle >> main.bundle.js
 
-// Show mobile menu
 $(function() {
-    $('.mobile-button').on('click', () => {
-        $('.wrapper').toggleClass('show-menu');
-    });
-});
 
-// Show mobile list on tabs
-$(function() {
-    $('.tabs ul li').on('click', (e) => {
-        let li = $(e.target).closest('li'),
-            li_list = $(li).siblings('li'),
-            li_first = li_list[0],
-            data_link = $(li).data('link');
+    $('body')
 
-        $(li).addClass('active').siblings('li').removeClass('active');
-        $('.tab[data-link="'+data_link+'"]').addClass('active').siblings().removeClass('active');
-        $('.tabs ul').toggleClass('open');
-        if ($(window).width() < 1024) {
-            $(li).insertBefore($(li_first));
-        }
+    // Show mobile menu
+        .on('click', '.js-header-mobile-button', function () {
+            $('.wrapper').toggleClass('is-show-menu');
     })
-});
 
-// Form input
-$(function() {
-   $('.for-input input').on('focus', (e) => {
-       let input = $(e.target),
-           parent = $(input).closest('.for-input');
-       $(parent).addClass('active');
-   });
-    $('.for-input input').on('blur', (e) => {
-        let input = $(e.target),
-            parent = $(input).closest('.for-input'),
-            label = $(parent).find('label'),
-            val = $(input).val();
+        // Show mobile list on tabs
+        .on('click', '.js-tab-link', function () {
+            let $li = $(this),
+                $liParent = $li.closest('.js-tabs-list'),
+                $liList = $liParent.find('.js-tab-link'),
+                dataLink = $li.data('link'),
+                $curTabs = $('.js-tab[data-link="' + dataLink + '"]');
+
+            $li.addClass('is-active');
+            $liList.not($li).removeClass('is-active');
+            $curTabs.addClass('is-active').closest('.js-tabs-content').find('.js-tab').not($curTabs).removeClass('is-active');
+            $liParent.toggleClass('open');
+        })
+
+        // Open/close modal
+        .on('click', '.js-popup-link', function () {
+            let $modal = $('.js-modal');
+            $modal.addClass('animation-showModal is-on');
+            $('.wrapper').addClass('is-show-modal');
+            setTimeout(() => $modal.removeClass('animation-showModal'), 500);
+        })
+        .on('click', '.overlay', function () {
+            let $modal = $('.js-modal');
+            $modal.addClass('animation-hideModal');
+            $('.wrapper').removeClass('is-show-modal');
+            setTimeout(() => $modal.removeClass('animation-hideModal is-on'), 500);
+        })
+
+        // Form input label
+        .on('focus', '.js-form-input', function () {
+            let $input = $(this),
+                $parent = $input.closest('.modal__for-row');
+            $parent.addClass('is-active');
+        })
+        .on('blur', '.js-form-input', function () {
+            let $input = $(this),
+                $parent = $input.closest('.modal__for-row'),
+                val = $input.val();
         if(val.length == 0) {
-            $(parent).removeClass('active');
+            $parent.removeClass('is-active');
         }
-    });
-});
+        })
 
-// Open/close modal
-$(function() {
-    $('.popup-link').on('click', (e) => {
-        let modal = $('.modal'),
-            overlay = $('.overlay');
-        $(modal).addClass('showModal on');
-        $('.wrapper').addClass('show-modal');
-        setTimeout(() => $(modal).removeClass('showModal'), 500);
-    });
-    $('.overlay').on('click', (e) => {
-        let modal = $('.modal'),
-            overlay = $('.overlay');
-        $(modal).addClass('hideModal');
-        $('.wrapper').removeClass('show-modal');
-        setTimeout(() => $(modal).removeClass('hideModal on'), 500);
-    });
-});
+        //  Remove error from input
+        .on('focus', '.js-form-input', function () {
+            $(this).removeClass('animation-shakeError is-error');
+        })
 
-// Form validation
-$(function(){
-    let removEr = () => {
-        $('input').focus(function(){
-            $(this).removeClass('shakeError error');
-        });
-    };
-    $('.reg-form').on('submit', (e) => {
-        let form = $(e.target),
-            login = $(form).find('input[name="login"]'),
-            pass = $(form).find('input[name="pass"]'),
+        // Simple form validation
+        .on('click', '.js-modal-submit-btn', function () {
+            let $btn = $(this),
+                $modal = $btn.closest('.js-modal'),
+                $formContainer = $modal.find('.modal__form-container'),
+                $input = $formContainer.find('.js-form-input'),
             error = false;
 
-        if(login.val().length < 3) {
-            $(login).addClass('shakeError error');
-            removEr();
-            error = true;
-        }
-        if(pass.val().length < 3) {
-            $(pass).addClass('shakeError error');
-            removEr();
-            error = true;
-        }
-        if (error) {
-            console.log('err')
-            return false;
-        } else {
-            console.log('ok')
+            $input.each(function () {
+                let $curInput = $(this),
+                    value = $curInput.val();
+                if (value.length < 3) {
+                    $curInput.addClass('animation-shakeError is-error');
+                    error = true;
+                }
+            });
+            if (!error) {
+                $modal.addClass('resault-active').removeClass('form-active')
         }
     })
-})
+});
