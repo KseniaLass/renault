800133bd33c76b073319470eccb39b2e6c49fc01
KDekho01 <ksenia.dekhonova@isobar.ru>
2017-01-17 18:22:17 +0300

    with for loop

--- sources/js/modules/module.stringFormat.js
+++ sources/js/modules/module.stringFormat.js
@@ -1,3 +1,19 @@
+/**
+ * Модуль обработки строк.
+ * Вспомогательные методы
+ * __setPhraseIndex(number, phrase) - функция склонения существительного. (number) - число. (phrase) - массив существительного в трех формах.-> получаем строку со значением. __setPhraseIndex(1, ['штука', 'штуки','штук']) -> 1 штука, 2 штуки итд
+ * __setNumberSeparate(number) - функция разделения по разрядам. (number) - число для преобразования. __setNumberSeparate(123456) -> 123 456
+ * __setTextDigit(digit, length) - функция перевода числовых значений в слова. (digit) - разряд числа из трех символов (123). (length) - число, количество разрядов.
+ * Рабочие методы
+ * __setDigits(value) - добавление пробелов между разрядами (см. __setNumberSeparate());
+ * __setCut(value) - замена слишком длинных строк "...". (value) -строка, значение.
+ * __setDeclension(value) - слонение существительного (см. __setNumberSeparate()).
+ * __setToText(value) - перевод числового значения в слова. Разбиваем значние на разряды (__setNumberSeparate()), в цикле отправляем каждый полученый разряд в функцию перевода в слова(см. __setTextDigit), записываем полученные строки в массив, расставляя существительные (тысяча, миллион) см(__setPhraseIndex(number, phrase), возвращаем полученую строку.
+ *
+ * __setResult(value, container) - вывод результатов. (value) - начение, полученое из рабочих методов. (container) - указывается в options
+ *
+ *
+ */
 class stringFormatBase {
 	constructor(options) {
 		this.options = options;
@@ -39,9 +55,10 @@ export default class stringFormat extends stringFormatBase {
 	// Helpers
 	__setPhraseIndex(number, phrase) {
 		var numString = number.toString(),
-			lastChar = numString.charAt(numString.length-1),
+			lastChar = numString.substr(numString.length-1),
+			twoLast = numString.substr(numString.length-2),
 			titles;
-		if (number >= 11 && number <=14){
+		if (twoLast >= 11 && twoLast <=14){
 			titles = phrase[2];
 		} else if (lastChar == 1) {
 			titles = phrase[0];
@@ -67,18 +84,22 @@ export default class stringFormat extends stringFormatBase {
 				a20 : ['', '', 'двадцать','тридцать','сорок','пятьдесят','шестьдесят','семьдесят','восемьдесят','девяносто'],
 				a100 : ['', 'сто','двести','триста','четыреста','пятьсот','шестьсот','семьсот','восемьсот','девятьсот']
 			},
-			first = digit.charAt(0),
-			sec = digit.charAt(1),
-			last = digit.charAt(digit.length - 1);
+			first = digit.charAt(digit.length-3),
+			sec = digit.charAt(digit.length-2),
+			last = digit.charAt(digit.length-1);
 
 		if(num <= 19) {
-			if((length == 2) && (num == 1 || num == 2)) {
+			if((length == 2) && (last == 1 || last == 2)) {
 				words = `${text.a2[num]}`;
 			} else {
 				words = `${text.a1[num]}`;
 			}
 		} else if (num >= 20 && num < 99) {
-			words = `${text.a20[first]} ${text.a1[sec]}`
+			if(last == 0) {
+				words = `${text.a20[sec]} ${text.a20[last]}`
+			} else {
+				words = `${text.a20[sec]} ${text.a1[last]}`
+			}
 		} else if (num >= 100) {
 			if(sec == 1) {
 				words = `${text.a100[first]} ${text.a1[sec+last]}`
@@ -115,12 +136,14 @@ export default class stringFormat extends stringFormatBase {
 			thousend,
 			million;
 		for(let i = 0; i <digit.length; i++) {
-			result.push(this.__setTextDigit(digit[i], digit.length));
+			//result.push(this.__setTextDigit(digit[i], digit.length));
 			if(digit.length == 2) {
+				result.push(this.__setTextDigit(digit[i], 2));
 				thousend = this.__setPhraseIndex(digit[0], ["тысяча", "тысячи", "тысяч"]);
 				phrase = `${result[0]} ${thousend} ${result[1]}`
 			} else if (digit.length == 3) {
-				thousend = this.__setPhraseIndex(digit[1], ["тысяча", "тысячи", "тысяч"]),
+				result.push(this.__setTextDigit(digit[i], 3));
+				thousend = this.__setPhraseIndex(digit[1], ["тысяча", "тысячи", "тысяч"]);
 				million = this.__setPhraseIndex(digit[0], ["миллион", "миллиона", "миллионов"]);
 				if(result[1] == '') {
 					phrase = `${result[0]} ${million} ${result[1]} ${result[2]}`;
@@ -128,8 +151,10 @@ export default class stringFormat extends stringFormatBase {
 					phrase = `${result[0]} ${million} ${result[1]} ${thousend} ${result[2]}`;
 				}
 			} else {
+				result.push(this.__setTextDigit(digit[i], 0));
 				phrase = result[0]
 			}
+			console.log(result)
 		}
 		return phrase;
 	}
