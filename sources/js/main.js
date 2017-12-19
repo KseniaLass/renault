d0217fd713067a818b3551905b22d738155849a2
KDekho01 <ksenia.dekhonova@isobar.ru>
2017-12-19 15:02:44 +0300

    change php

--- sources/js/main.js
+++ sources/js/main.js
@@ -2,172 +2,174 @@
 
 
 $(document).ready(function(){
-    // Selectbox
-    $('.js-dropdown').click(function(e){
-        e.stopPropagation();
-        //e.preventDefault();
-        var $list = $(this).closest('.selectbox').find('.selectbox__dropdown');
-        $(this).closest('.form__row').removeClass('error');
-        if($(this).hasClass('is-open')) {
-            $list.removeClass('is-active');
-            $(this).removeClass('is-open');
-        } else {
-            $list.addClass('is-active');
-            $(this).addClass('is-open');
-        }
+	// Selectbox
+	$('.js-dropdown').click(function(e){
+		e.stopPropagation();
+		//e.preventDefault();
+		var $list = $(this).closest('.selectbox').find('.selectbox__dropdown');
+		$(this).closest('.form__row').removeClass('error');
+		if($(this).hasClass('is-open')) {
+			$list.removeClass('is-active');
+			$(this).removeClass('is-open');
+		} else {
+			$list.addClass('is-active');
+			$(this).addClass('is-open');
+		}
 
-    });
-    $('body').click(function(e){
-        $('.selectbox__dropdown').removeClass('is-active');
-        $('.js-dropdown').removeClass('is-open')
-    });
-    $('.selectbox__dropdown').click(function(e){
-        e.stopPropagation();
-        var value = $(e.target).text();
-        var $input = $(this).closest('.selectbox').find('.selectbox__input');
-        $input.text(value);
-        $(this).removeClass('is-active');
+	});
+	$('body').click(function(e){
+		$('.selectbox__dropdown').removeClass('is-active');
+		$('.js-dropdown').removeClass('is-open');
+	});
+	$('.selectbox__dropdown').click(function(e){
+		e.stopPropagation();
+		var value = $(e.target).text();
+		var id = $(e.target).data('salon');
+		var $input = $(this).closest('.selectbox').find('.selectbox__input');
+		$input.text(value).attr('data-salon', id);
+		$(this).removeClass('is-active');
 
-        var tabs = $(this).data('tabs');
+		var tabs = $(this).data('tabs');
 
-        if(tabs === 'contacts') {
-            var target = $(e.target).data('target');
-            $('.contacts__addr').removeClass('is-active');
-            $('.contacts__form').find(target).addClass('is-active');
-        }
+		if(tabs === 'contacts') {
+			var target = $(e.target).data('target');
+			$('.contacts__addr').removeClass('is-active');
+			$('.contacts__form').find(target).addClass('is-active');
+		}
 
-    });
+	});
 
-    // Input label
-    $('input[type="tel"]').focus(function(e){
-        $(this).closest('.form__row').removeClass('error');
-        $(this).closest('.form__input').addClass('is-focus');
-    });
-    $('input[type="tel"]').blur(function(e){
-        if($(this).val().length === 0 || $(this).val() === '+7 (___) ___-__-__') {
-            $(this).closest('.form__input').removeClass('is-focus');
-        }
-    });
+	// Input label
+	$('input[type="tel"]').focus(function(e){
+		$(this).closest('.form__row').removeClass('error');
+		$(this).closest('.form__input').addClass('is-focus');
+	});
+	$('input[type="tel"]').blur(function(e){
+		if($(this).val().length === 0 || $(this).val() === '+7 (___) ___-__-__') {
+			$(this).closest('.form__input').removeClass('is-focus');
+		}
+	});
 
-    //Masked Input
-    $('input[type="tel"]').mask('+7 (999) 999-99-99',{autoclear: false});
+	//Masked Input
+	$('input[type="tel"]').mask('+7 (999) 999-99-99',{autoclear: false});
 
-    //Video tabs
-    $('.js-video-tabs').click(function(e){
-        e.preventDefault();
-        var target = $(this).data('video');
-        $('.js-video-tabs').removeClass('is-active');
-        $(this).addClass('is-active');
-        $('#video').attr('src', 'https://www.youtube.com/embed/'+target);
-    });
+	//Video tabs
+	$('.js-video-tabs').click(function(e){
+		e.preventDefault();
+		var target = $(this).data('video');
+		$('.js-video-tabs').removeClass('is-active');
+		$(this).addClass('is-active');
+		$('#video').attr('src', 'https://www.youtube.com/embed/'+target);
+	});
 
-    // Popup
-    $('.js-close-popup').click(function(e) {
-        e.preventDefault();
-        $('.popup').removeClass('is-active');
-        $('html, body').css('overflow', 'auto');
-    });
-    $('.js-open-popup').click(function(e){
-        e.preventDefault();
-       var popup = $(this).data('popup');
-        showPopup(popup);
-    });
+	// Popup
+	$('.js-close-popup').click(function(e) {
+		e.preventDefault();
+		$('.popup').removeClass('is-active');
+		$('html, body').css('overflow', 'auto');
+	});
+	$('.js-open-popup').click(function(e){
+		e.preventDefault();
+	   var popup = $(this).data('popup');
+		showPopup(popup);
+	});
 
-    // Maps
-    ymaps.ready(init);
-    var myMap,
-        myPlacemark;
-    function init() {
-        myMap = new ymaps.Map('map', {
-            center: [59.952529, 30.374233],
-            zoom: 9
-        });
-        var coords = {
-            'leninsky': { ballon: 'Ленинский пр., 101, к. 3', coord: [59.850988, 30.224321]},
-            'rustavely': { ballon: 'ул. Руставели, д. 31, корп. 3', coord: [60.008378, 30.436575]},
-            'sof': { ballon: 'ул. Софийская, 87', coord: [59.844981, 30.430035]}
-        };
+	// Maps
+	ymaps.ready(init);
+	var myMap,
+		myPlacemark;
+	function init() {
+		myMap = new ymaps.Map('map', {
+			center: [59.952529, 30.374233],
+			zoom: 9
+		});
+		var coords = {
+			'leninsky': { ballon: 'Ленинский пр., 101, к. 3', coord: [59.850988, 30.224321]},
+			'rustavely': { ballon: 'ул. Руставели, д. 31, корп. 3', coord: [60.008378, 30.436575]},
+			'sof': { ballon: 'ул. Софийская, 87', coord: [59.844981, 30.430035]}
+		};
 
-        var myCollections = new ymaps.GeoObjectCollection(null,{});
+		var myCollections = new ymaps.GeoObjectCollection(null,{});
 
-        for(var prop in coords) {
-            console.log(coords[prop].ballon)
-            //yellowCollection.add(new ymaps.Placemark(yellowCoords[i]));
-            myCollections.add(new ymaps.Placemark(coords[prop].coord,
-                {
-                    hintContent: 'Москва!',
-                    balloonContent: coords[prop].ballon
-                },
-                {
-                    iconLayout: 'default#image',
-                    iconImageHref: '../img/logomap.png',
-                    iconImageSize: [40, 40]
-                })
-            )
+		for(var prop in coords) {
+			//yellowCollection.add(new ymaps.Placemark(yellowCoords[i]));
+			myCollections.add(new ymaps.Placemark(coords[prop].coord,
+				{
+					hintContent: 'ПЕТРОВСКИЙ АВТОЦЕНТР',
+					balloonContent: coords[prop].ballon
+				},
+				{
+					iconLayout: 'default#image',
+					iconImageHref: '../img/logomap.png',
+					iconImageSize: [40, 40]
+				})
+			)
 
-        }
+		}
 
-        myMap.geoObjects.add(myCollections);
-        myMap.controls.remove('rulerControl');
-        myMap.controls.remove('searchControl');
-        myMap.controls.remove('trafficControl');
-        myMap.controls.remove('layoutControl');
-        //myMap.behaviors.disable('scrollZoom');
-    }
-    function showPopup(popup) {
-        $('.popup').removeClass('is-active');
-        $('#'+popup).addClass('is-active');
-        $('html, body').css('overflow', 'hidden');
-    }
+		myMap.geoObjects.add(myCollections);
+		myMap.controls.remove('rulerControl');
+		myMap.controls.remove('searchControl');
+		myMap.controls.remove('trafficControl');
+		myMap.controls.remove('layoutControl');
+		//myMap.behaviors.disable('scrollZoom');
+	}
+	function showPopup(popup) {
+		$('.popup').removeClass('is-active');
+		$('#'+popup).addClass('is-active');
+		$('html, body').css('overflow', 'hidden');
+	}
 
-    // Ajax
+	// Ajax
 
-    $('.js-ajax').submit(function(e){
-        e.preventDefault();
-        var $salon = $(this).find('.selectbox__input'),
-            salonValue = $salon.text();
+	$('.js-ajax').submit(function(e){
+		e.preventDefault();
+		var $salon = $(this).find('.selectbox__input'),
+			salonValue = $salon.text(),
+			salonID = $salon.attr('data-salon');
 
-        var $phone = $(this).find('input[name="phone"]'),
-            phoneValue = $phone.val();
+		var $phone = $(this).find('input[name="phone"]'),
+			phoneValue = $phone.val();
 
-        var form = $(this).find('input[name="form"]').val();
+		var form = $(this).find('input[name="form"]').val();
 
-        var error;
+		var error;
 
-        if (salonValue === 'Выберите салон' || salonValue === '') {
-            $salon.closest('.form__row').addClass('error');
-            error = true;
-        }
-        if (phoneValue === '+7 (___) ___-__-__' || phoneValue === '') {
-            $phone.closest('.form__row').addClass('error');
-            error = true;
-        }
-        if(error) {
-            return false
-        }
+		if (salonValue === 'Выберите салон' || salonValue === '') {
+			$salon.closest('.form__row').addClass('error');
+			error = true;
+		}
+		if (phoneValue === '+7 (___) ___-__-__' || phoneValue === '') {
+			$phone.closest('.form__row').addClass('error');
+			error = true;
+		}
+		if(error) {
+			return false;
+		}
 
-        var data = {
-            salon: salonValue,
-            phone: phoneValue,
-            form: form
-        };
+		var data = {
+			salon: salonValue,
+			phone: phoneValue,
+			form: form,
+			id: salonID
+		};
 
-        $.ajax({
-            type: "POST",
-            url: 'mail.php',
-            data: data,
-            success: function success(data) {
-                if (data == 'ok') {
-                    showPopup('success-popup');
-                    $('input[type="tel"]').val('').trigger('blur');
-                }
-            },
-            error: function error() {
-                showPopup('fail-popup');
-                $('input[type="tel"]').val('').trigger('blur');
-            }
-        });
+		$.ajax({
+			type: "POST",
+			url: 'mail.php',
+			data: data,
+			success: function success(data) {
+				if (data == 'ok') {
+					showPopup('success-popup');
+					$('input[type="tel"]').val('').trigger('blur');
+				}
+			},
+			error: function error() {
+				showPopup('fail-popup');
+				$('input[type="tel"]').val('').trigger('blur');
+			}
+		});
 
-    })
+	})
 
 });
--- sources/mail.php
+++ sources/mail.php
@@ -12,6 +12,27 @@ function check_phone($phone){
 $form = trim($_POST['form']);
 $phone = trim($_POST['phone']);
 $salon = trim($_POST['salon']);
+$id = trim($_POST['id']);
+
+$name = trim($_POST['name']) || '';
+$email = trim($_POST['email']) || '';
+
+switch ($id) {
+    case 1:
+        $recipients = 'antipov.anton@petrovskiy.ru , sorokin.dmitriy@petrovskiy.ru, merzhoeva.anna@petrovskiy.ru';
+        $steamer = 'ОП Софийская spa1@petrovskiy.ru';
+        break;
+
+    case 2:
+        $recipients = 'abramichev.andrej@petrovskiy.ru , novikov.sergey@petrovskiy.ru, merzhoeva.anna@petrovskiy.ru';
+        $steamer = 'ОП Ленинский spa2@petrovskiy.ru';
+        break;
+
+    case 3:
+        $recipients = 'klimenko.alexey@petrovskiy.ru , tyurin.maksim@petrovskiy.ru, merzhoeva.anna@petrovskiy.ru';
+        $steamer = 'ОП Руставели spa3@petrovskiy.ru';
+        break;
+}
 
 if ( (check_phone($phone)) && (strlen($salon) > 2) ) {
     echo('ok');
@@ -20,14 +41,38 @@ if ( (check_phone($phone)) && (strlen($salon) > 2) ) {
     die();
 }
 
-$msg = "Заявка с формы: '" . $form . "'" . "\nСалон: " . $salon . "\nТелефон: " . $phone;
+$msg = <<<EOD
+    Лендинг: 
+    Скидка 10 000 рублей.
+    
+    Имя:
+    $name
+    
+    Email:
+    $email
+    
+    Телефон:
+    $phone
+    
+    Салон:
+    $salon
+    
+    Кнопка на которую нажали:
+    $form
+    
+    Служба:
+    Отдел продаж новых автомобилей
+EOD;
+
+
+//$msg = "Лендинг: Скидка 10 000 рублей. \n\nИмя: Заявка с формы: '" . $form . "'" . "\nСалон: " . $salon . "\nТелефон: " . $phone;
 
 file_put_contents('log.txt', $msg, FILE_APPEND);
 
 file_put_contents('log.txt', $output, FILE_APPEND);
 
-$headers = 'From: order@landing.ru';
+$headers = 'From: ' . $steamer;
 $date = date('d-j-Y G:i');
-$subject = 'Новая заявка с лендинга Петровский автоцентр от ' . $date;
-mail('ksenialass@gmail.com', $subject, $msg, $headers);
+$subject = 'Новая заявка с лендинга "Скидка 10 000 рублей" от ' . $date;
+mail($recipients, $subject, $msg, $headers);
 ?>
